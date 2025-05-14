f3fe0528341169588e576d719f220ee71f085b15
Parul Pandey <parulss1606@gmail.com>
2025-05-14 14:16:01 +0530

    first commit

---
+++ src/test/java/com/ticket_management_system/database_api/DatabaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticket_management_system.database_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
